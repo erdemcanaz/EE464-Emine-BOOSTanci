a0a32425b4ab958923e280968eda2b0557cf1824
Erdem <erdemcanaz@gmail.com>
2024-06-14 02:44:57 +0300

    Simulation results

--- plotter/plot_bar.py
+++ plotter/plot_bar.py
@@ -1,11 +1,14 @@
 import matplotlib.pyplot as plt
 
 # Sample data
-x = ['Pin', 'Pout', 'Psnubber', 'Poutput_diode', 'Pcore', 'Pmosfet',  'Pwinding', 'Pdiode_snubber','Pother']
-y = [77.61, 62.782, 3.579, 3.33, 2.541, 2.389, 0.3, 0.21, 2.51]
+x = ['Pin', 'Pout', 'Psnubber', 'Poutput_diode', 'Pcore', 'Pmosfet',  'Pwinding', 'Pdiode_snubber','Pshunt', 'Pesr','Pother']
+y = [74.95, 58.41, 2.53, 2.91, 2.59 , 0.92, 0.23, 0.16, 1.8, 3.3, 2.2]
+
+x = [x for _, x in sorted(zip(y, x), reverse=True)]
+y = sorted(y, reverse=True)
 
 # Plotting the bar chart
-plt.bar(x, y)
+plt.bar(x, y, color=['darkblue'] + ['lightblue'] * (len(x) - 1))
 for i, v in enumerate(y):
     plt.text(i, v, f"{v:.1f}W - "+str(round((v / max(y)) * 100, 2)) + '%', ha='center', va='bottom')
 #plt.xticks(rotation=90)
